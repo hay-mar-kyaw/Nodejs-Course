83243abbf4323ddc705e92e12bb61fa7695394b1
hay-mar-kyaw <mandalaytvsindiamotorcycles@gmail.com>
2026-01-19 16:56:39 +0630

    global object

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-console.log("Hello, World!");
-console.log("This is the index.js file.");
+let interval = setInterval(() => {
+  console.log('This will run every second');
+}, 1000);
+
+setTimeout(() => {
+  clearInterval(interval);
+  console.log('Interval cleared after 5 seconds');
+}, 5000);
+
+// Additional code can be added here if needed
+console.log(global.document)
